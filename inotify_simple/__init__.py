d1db2f8e93e43be7fe89f38d6d2b5944fa5773cb
Chris Billington <chrisjbillington@gmail.com>
2016-01-11 23:38:35 +1100

    Fix non relative import for __version__

--- inotify_simple/__init__.py
+++ inotify_simple/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 from .inotify_simple import  *
 try:
-    from __version__ import __version__
+    from .__version__ import __version__
 except ImportError:
     __version__ = None
