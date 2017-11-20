517cc521e2b1c17c7a5b0b0688bcb9f866a75895
Chris Billington <chrisjbillington@gmail.com>
2017-11-20 14:26:46 -0500

    got docs working on PyPI again

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@
 # If the package is not registered with PyPI yet, do so with:
 #     python setup.py register
 
+import pypandoc
+with open('README.rst', 'w') as f:
+    f.write(pypandoc.convert('README.md', 'rst', format='markdown'))
+
 import os
 from distutils.core import setup
 
-__version__ = '1.1.2'
+__version__ = '1.1.5'
 
-DESCRIPTION = \
-    """A simple wrapper around inotify. No fancy bells and whistles, just a
-    literal wrapper with ctypes. Only 118 lines of code!
-"""
+DESCRIPTION = ("A simple wrapper around inotify. No fancy bells and whistles, " +
+               "just a literal wrapper with ctypes. Only 118 lines of code!")
 
 # Auto generate a __version__ package for the package to import
 with open(os.path.join('inotify_simple', '__version__.py'), 'w') as f:
@@ -22,6 +24,7 @@ with open(os.path.join('inotify_simple', '__version__.py'), 'w') as f:
 setup(name='inotify_simple',
       version=__version__,
       description=DESCRIPTION,
+      long_description=open('README.rst').read(),
       author='Chris Billington',
       author_email='chrisjbillington@gmail.com',
       url='https://github.com/chrisjbillington/inotify_simple',
