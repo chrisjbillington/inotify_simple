c1b7705ecedf365f4fc999c0597e3fc06d01f781
Jan Beich <jbeich@FreeBSD.org>
2021-01-15 11:08:43 +0000

    Try libinotify before libc
    inotify isn't supported by libc/kernel on BSDs but emulated via kqueue.
    
    >>> import inotify_simple
    >>> inotify_simple.INotify()
    Traceback (most recent call last):
      File "<stdin>", line 1, in <module>
      File "/usr/local/lib/python3.9/site-packages/inotify_simple.py", line 91, in __init__
        FileIO.__init__(self, _libc_call(_libc.inotify_init1, flags), mode='rb')
      File "/usr/local/lib/python3.9/ctypes/__init__.py", line 387, in __getattr__
        func = self.__getitem__(name)
      File "/usr/local/lib/python3.9/ctypes/__init__.py", line 392, in __getitem__
        func = self._FuncPtr((name_or_ordinal, self))
    AttributeError: Undefined symbol "inotify_init1"

--- inotify_simple.py
+++ inotify_simple.py
@@ -82,7 +82,7 @@ class INotify(FileIO):
                 manually with ``os.read(fd)``) to raise ``BlockingIOError`` if no data
                 is available."""
         try:
-            libc_so = find_library('c')
+            libc_so = find_library('inotify') or find_library('c')
         except RuntimeError: # Python on Synology NASs raises a RuntimeError
             libc_so = None
         global _libc; _libc = _libc or CDLL(libc_so or 'libc.so.6', use_errno=True)
